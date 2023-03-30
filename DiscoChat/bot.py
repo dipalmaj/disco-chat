01324ffa3941c9c31b0e53cf7703d5ef2cd818c2
Gio DiPalma <dipalmaj@users.noreply.github.com>
2023-03-30 18:21:07 -0400

    Added new python package, reworked the openai code to exist inside new module and refactored existing code to reduce redundancy.

--- DiscoChat/bot.py
+++ DiscoChat/bot.py
@@ -8,6 +8,10 @@ from discord.ext import commands
 import argparse
 import atexit
 
+from utils.chatter_box import ChatterBox
+
+version = "0.0.1"
+
 load_dotenv()  # load environment variables from .env file
 
 openai.api_key = os.getenv("OPENAI_API_KEY")  # set OpenAI API key from .env file
@@ -16,7 +20,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)  # create a Discord bot instance
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)  # create a Discord bot instance
 
 try:
     discord_user_id = int(os.getenv("DISCORD_USER_ID"))
@@ -24,41 +28,38 @@ except ValueError:
     print("Unable to cast discord id properly. Verify value is setup properly")
     discord_user_id = "NotSet"
 
-model = "text-davinci-003"
-chat_model = "gpt-3.5-turbo"
-# model = "text-davinci-002"
-
-chat_agent = "You are a helpful chat agent helping software developers with questions"
+chatter = ChatterBox()
 
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
     print(f"Application ID: {bot.application_id}")
-    print(f"Chat agent: {chat_agent}")
+    print(f"Chat agent: {chatter.chat_agent}")
 
 
 @bot.command()
-async def describe_chat_agent(ctx):
-    print(chat_agent)
-    await ctx.send(f"DiscoChat Agent is set as \"{chat_agent}\"")
+async def describe_chat_agent(ctx: commands.Context):
+    await ctx.send(f"DiscoChat Agent is set as \"{chatter.chat_agent}\"")
 
 
-@bot.command()
-async def set_chat_agent(ctx, *, chat_agent_description):
-    if not permission_check(ctx.author.roles, ['Admin']):
-        await ctx.send(f"So sorry! I can't do that see your admin for privileges")
-    else:
-        global chat_agent
-        chat_agent = chat_agent_description
-        await ctx.send(f"Attempting to update agent\nAgent set to \"{chat_agent}\"")
+@bot.command(hidden=True)
+async def set_chat_agent(ctx: commands.Context, *, chat_agent_description):
+    chatter.chat_agent(chat_agent_description)
+    await ctx.send(f"Attempting to update agent\nAgent set to \"{chatter.chat_agent}\"")
 
 
-@bot.command()
-async def check_user(ctx):
+@bot.command(hidden=True)
+async def check_user(ctx: commands.Context):
+    """
+    Playing around with permissions and handling around guilds/roles
+    """
     channel = ctx.channel.name
-    if discord_user_id == ctx.author.id:
-        await ctx.send(f'Hello owner!')
+
+    if await bot.is_owner(ctx.author):
+        await ctx.send(f'Hello Owner')
+    elif discord_user_id == ctx.author.id:
+        await ctx.send(f'Hello Developer!')
     elif permission_check(ctx.author.roles, ['Admin', 'Tester']):
         await ctx.send(f'Checking')
         user = discord.utils.get(bot.get_all_members(), id=ctx.author.id)
@@ -68,92 +69,54 @@ async def check_user(ctx):
 
 
 @bot.command()
-async def chat(ctx, *, question):
-    response = openai.ChatCompletion.create(
-        model=chat_model,
-        messages=[
-            {"role": "system", "content": chat_agent},
-            {"role": "user", "content": question}
-        ]
-    )
+async def chat(ctx: commands.Context, *, question):
+    response = chatter.send_to_agent(question)
     await ctx.send(response.choices[0].message.content)
 
 
 @bot.command()
-async def sassy(ctx, *, question):
-    response = openai.ChatCompletion.create(
-        model=chat_model,
-        messages=[
-            {"role": "system", "content": "Respond as if you're a sassy teenage girl from Los Angeles. You've never "
-                                          "worked a day in your life and have everything you want brought to you by a "
-                                          "full staff"},
-            {"role": "user", "content": question}
-        ]
-    )
+async def sassy(ctx: commands.Context, *, question):
+    response = chatter.send_to_sassy(question)
     await ctx.send(response.choices[0].message.content)
 
 
 @bot.command()
-async def ask(ctx, *, question):
-    response = openai.ChatCompletion.create(
-        model=chat_model,
-        messages=[
-            {"role": "user", "content": question}
-        ]
-    )
+async def ask(ctx: commands.Context, *, question):
+    response = chatter.send(question)
     await ctx.send(response.choices[0].message.content)
 
 
 @bot.command()
-async def artsy(ctx, *, question):
-    response = openai.ChatCompletion.create(
-        model=chat_model,
-        messages=[
-            {"role": "system", "content": "Generate midjourney prompts by supplying very specific, detailed words "
-                                          "capable of relaying imagery to nearly everyone"},
-            # {"role": "system", "content": "Respond as a full tenured art instructor who specializes in rembrandt and "
-            #                               "picaso"},
-            {"role": "user", "content": "imagine yourself as a world renown artist such as Rembrant, Picaso and Monet"
-                                        "using a wide vocabulary to accurately depict their painting "
-                                        "to an audience that can't see it "},
-            {"role": "user", "content": question}
-        ]
-    )
+async def artsy(ctx: commands.Context, *, question):
+    response = chatter.send_to_artsy(question)
     await ctx.send(response.choices[0].message.content)
 
 
 @bot.command()
-async def code(ctx, *, question):
-    # print(f"Received question: {question}")
-    response = openai.Completion.create(
-        model=model,
-        prompt=question,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+async def code(ctx: commands.Context, *, question: str):
+    """
+    This command takes a string that will be passed to AI and returned formatted as code
+
+    :param ctx: discord's commands.Context object
+    :param question: Message that will be sent to AI
+    """
+    response = chatter.request_code_snippet(question)
     await ctx.send("```\n" + response.choices[0].text + "\n```")
 
 
-# Attempted to make use of hybrid_command which should allow commands to be registered to slash;
-# however, you need to sync the CommandTree and that requires client...
-# Didn't get it to work... discord.app_commands.CommandTree(client).sync()
-@bot.hybrid_command()
-async def about(ctx):
+@bot.command()
+async def about(ctx: commands.Context):
     await ctx.send("DiscoChat is a basic chatGPT relay.\n"
-                   f"Code uses model: {model}\n"
-                   "`!code` will respond using code block formatting\n"
-                   f"Ask and Chat make use of chat model: {chat_model}\n"
-                   "`!describe_chat_agent` returns the currently set value for the chat agent's system message\n"
-                   "`!set_chat_agent` updates chat agent's system message\n"
-                   "`!ask` provides chat model answer with no system message\n"
-                   "`!sassy` provides chat from a opinionated, spoiled teenage girl\n"
-                   "`!chat` provides chat model answers with simple system message")
+                   f"Version {version}\n"
+                   f"Code uses model: {chatter.model}\n"
+                   "usage: !help <cmd> or @DiscoChat help <cmd>")
 
 
-# TODO see about adding a sign-off for the bot to post
 def graceful_shutdown():
+    """
+    stub for some alerting on shutdowns
+        TODO see about adding a sign-off for the bot to post
+    """
     try:
         print("Shutting down gracefully...")
         # Perform any necessary cleanup operations here.
@@ -165,8 +128,6 @@ def graceful_shutdown():
         sys.exit(0)
 
 
-# Not fully flushed out, but feel like functionality will be useful across functions
-# TODO think about design of this function, may be easier for called to just pass ctx
 def permission_check(author_roles, expected_roles):
     for role in expected_roles:
         if not discord.utils.get(author_roles, name=role):
---
+++ DiscoChat/utils/chatter_box.py
@@ -0,0 +1,99 @@
+import openai
+
+
+sassy_agent = "Respond as if you're a sassy teenage girl from Los Angeles. " \
+                  "You've never worked a day in your life " \
+                  "and have everything you want brought to you by a full staff"
+
+
+class ChatterBox:
+    """
+    Contains chat related helper functionality
+        aiming eventually to have some tailored functionality
+
+        TODO
+         - model updating and checks - older text model "text-davinci-002"
+         - logging settings
+         - track certain data points
+    """
+
+    def __init__(self):
+        self._chat_agent = "You are a helpful chat agent helping software developers with questions"
+        self.chat_model = "gpt-3.5-turbo"
+        self.model = "text-davinci-003"
+        self._current_messages = list()
+        self._messages_sent = True
+
+    @property
+    def chat_agent(self):
+        return self._chat_agent
+
+    @chat_agent.setter
+    def chat_agent(self, agent_info):
+        # TODO add ability to enable logging or tracking of used agents
+        self._chat_agent = agent_info
+
+    def purge_messages(self):
+        if not self._messages_sent:
+            # TODO better logging options and handling
+            print(f"current_messages is being emptied without being sent\n{self._current_messages}")
+        self._current_messages = list()
+
+    def create_system_message(self, agent=None):
+        if not agent:
+            agent = self._chat_agent
+        if self._current_messages:
+            self.purge_messages()
+        self._current_messages.append({"role": "system", "content": agent})
+
+    def create_chat_message(self, message):
+        self._current_messages.append({"role": "user", "content": message})
+
+    def get_chat_response(self):
+        response = openai.ChatCompletion.create(
+            model=self.chat_model,
+            messages=self._current_messages
+        )
+        self._messages_sent = True
+        return response
+
+    def send(self, question):
+        """
+        Used for sending a message to AI without any system level message being setup
+        :param question:
+        :return:
+        """
+        self.create_chat_message(question)
+        return self.get_chat_response()
+
+    def send_to_agent(self, questions, agent: str = None):
+        self.create_system_message(agent)
+        if isinstance(questions, list):
+            [self.create_chat_message(question) for question in questions]
+        else:
+            self.create_chat_message(questions)
+        return self.get_chat_response()
+
+    def send_to_sassy(self, question):
+        return self.send_to_agent(question, sassy_agent)
+
+    def send_to_artsy(self, question):
+        artsy_agent = "Generate midjourney prompts by supplying very specific, detailed words "\
+                       "capable of relaying imagery to nearly everyone"
+        questions = [
+            "imagine yourself as a world renown artist such as Rembrandt, Picaso and Monet"
+            "using a wide vocabulary to accurately depict their painting "
+            "to an audience that can't see it ",
+            question
+        ]
+        return self.send_to_agent(questions, artsy_agent)
+
+    def request_code_snippet(self, prompt):
+        return openai.Completion.create(
+            model=self.model,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
